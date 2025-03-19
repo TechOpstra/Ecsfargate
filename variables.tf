e521d1fd17106a3b67b7db601f299d2876e441b3
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 17:51:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -48,6 +48,11 @@ variable "ecs_execution_role_name" {
   default     = "ecs-execution-role"
 }
 
+variable "ecs_task_role_arn" {
+  description = "The ARN of the ECS task role"
+  type        = string
+}
+
 variable "ecs_task_role_name" {
   description = "Name of the ECS Task Role"
   type        = string
