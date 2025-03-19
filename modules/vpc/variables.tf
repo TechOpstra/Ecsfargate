10db83f326c0dbcad5b09bb6ec291a6288c135c1
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 22:51:57 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -3,13 +3,22 @@ variable "vpc_cidr_block" {
   type        = string
 }
 
-variable "subnet_cidr_block" {
-  description = "The CIDR block for the subnet"
+variable "subnet_cidr_block_1" {
+  description = "The CIDR block for the first subnet"
   type        = string
 }
 
-variable "availability_zone" {
-  description = "The availability zone for the subnet"
+variable "subnet_cidr_block_2" {
+  description = "The CIDR block for the second subnet"
   type        = string
 }
 
+variable "availability_zone_1" {
+  description = "The availability zone for the first subnet"
+  type        = string
+}
+
+variable "availability_zone_2" {
+  description = "The availability zone for the second subnet"
+  type        = string
+}
