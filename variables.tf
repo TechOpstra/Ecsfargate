f453a578a2a1b858c576ded5a921be66b92d9597
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 18:36:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -48,17 +48,6 @@ variable "ecs_execution_role_name" {
   default     = "ecs-execution-role"
 }
 
-variable "ecs_task_role_arn" {
-  description = "The ARN of the ECS task role"
-  type        = string
-}
-
-variable "ecs_task_role_name" {
-  description = "Name of the ECS Task Role"
-  type        = string
-  default     = "ecs-task-role"
-}
-
 variable "alb_name" {
   description = "Name of the Application Load Balancer"
   type        = string
