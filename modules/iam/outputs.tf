d045470383f051a4c4b8da0a36b4444443ddcb15
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 18:32:03 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -2,8 +2,3 @@ output "ecs_execution_role_arn" {
   description = "The ARN of the ECS execution role"
   value       = aws_iam_role.ecs_execution_role.arn
 }
-
-output "ecs_task_role_arn" {
-  description = "The ARN of the ECS task role"
-  value       = aws_iam_role.ecs_task_role.arn
-}
