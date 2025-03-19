211dbc32312af7257d8486280b00ea63ef64603b
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 18:59:12 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -65,3 +65,17 @@ output "alb_dns_name" {
   description = "DNS name of the alb"
   value       = module.alb.alb_dns_name
 }
+output "alb_id" {
+  description = "The ID of the application load balancer"
+  value       = aws_lb.demo_apps_lb.id
+}
+
+output "patient_tg_arn" {
+  description = "The ARN of the patient service target group"
+  value       = aws_lb_target_group.patient_tg.arn
+}
+
+output "appointment_tg_arn" {
+  description = "The ARN of the appointment service target group"
+  value       = aws_lb_target_group.appointment_tg.arn
+}
