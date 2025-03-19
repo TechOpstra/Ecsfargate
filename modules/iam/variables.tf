f1d48e8294a91a1944827c4c4413c8f393f95780
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 19:13:58 +0530

    Update variables.tf

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -4,3 +4,8 @@ variable "ecs_execution_role_name" {
   default     = "ecs-execution-role"
 }
 
+variable "ecs_task_role_name" {
+  description = "Name of the ECS Task Role"
+  type        = string
+  default     = "ecs-task-role"
+}
