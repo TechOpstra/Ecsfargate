bef9dc027d8afcd439d5968c565830c5232fdadc
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 18:54:19 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -27,3 +27,42 @@ output "alb_sg_id" {
   description = "The ID of the Application Load Balancer Security Group"
   value       = aws_security_group.alb_sg.id
 }
+
+output "ecs_execution_role_arn" {
+  description = "The ARN of the ECS execution role"
+  value       = module.ecs.ecs_execution_role.arn
+}
+
+output "ecs_cluster_name" {
+  value = module.ecs.ecs_cluster_name
+}
+
+output "ecs_cluster_id" {
+  value = module.ecs.ecs_cluster_id
+}
+
+output "ecs_task_definition_arn" {
+  value = module.ecs.ecs_task_definition_arn
+}
+
+output "patient_service_id" {
+  value = module.ecs.patient_service_id
+}
+
+output "appointment_service_id" {
+  value = module.ecs.appointment_service_id
+}
+
+output "patient_service_ip" {
+  value = module.ecs.patient_service_ip
+}
+
+output "appointment_service_ip" {
+  value = module.ecs.appointment_service_ip
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the alb"
+  value       = module.alb.alb_dns_name
+}
+
