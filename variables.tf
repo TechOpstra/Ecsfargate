667a3d5ec95bed1c578c62d80896efe22218d471
Harish <107391575+8555936327@users.noreply.github.com>
2025-03-19 23:05:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,16 +10,28 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr_block" {
-  description = "The CIDR block for the subnet"
+variable "subnet_cidr_block_1" {
+  description = "The CIDR block for the first subnet"
   type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "availability_zone" {
-  description = "The availability zone for the subnet"
+variable "subnet_cidr_block_2" {
+  description = "The CIDR block for the second subnet"
+  type        = string
+  default     = "10.0.2.0/24
+}
+
+variable "availability_zone_1" {
+  description = "The availability zone for the first subnet"
   type        = string
   default     = "us-east-1a"
+}
+
+variable "availability_zone_2" {
+  description = "The availability zone for the second subnet"
+  type        = string
+  default     = "us-east-1b"
 }
 
 variable "ecs_cluster_name" {
